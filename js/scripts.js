3ab87c975aa8fb027efd220fb2ec0ce8944a83ec
first-name second-namegit config --global user.name Alice <njambialice25@gmail.com>
2018-05-04 12:21:27 +0300

    added a new spec to return pong when 5 is entered or pp when a number that doesn't apply to the rules is entered

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,15 @@ pingPongi = function(pp) {
  if (pp % 15 === 0){
     return"pingpong"
 
+
 } else if (pp % 3 === 0){
   return"ping"
 
+} else if (pp % 5 === 0) {
+  return "pong";
+  
+}else{
+  return "pp";
+}
 
 }
-};
